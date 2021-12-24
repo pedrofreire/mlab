44dc864db44807c2d11e6ec495c0722adf2ac53b
Pedro Freire <pedrofreirex@gmail.com>
2021-12-24 01:00:10 -0800

    Add call in file

--- w1d3/important.py
+++ w1d3/important.py
@@ -1,5 +1,13 @@
 import gin
 
 @gin.configurable
-def loss(x):
+def loss(x=None):
     return (x - 3.0)**2
+
+gin_config_string = """
+loss.x = (-1, 0, 2.0, 3.0, 10.0)
+"""
+
+if __name__ == '__main__':
+    gin.parse_config(gin_config_string)
+    loss()
